ac121a54de9063cbd7f287f1d092c36455a0f58e
Rodrigo <rd.bogiel@gmail.com>
2015-07-11 10:58:46 -0300

    Commit

--- FontesVS/CXA/Frente_CXA/Frente_CXA/Program.cs
+++ FontesVS/CXA/Frente_CXA/Frente_CXA/Program.cs
@@ -16,7 +16,8 @@ namespace Frente_CXA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
+            //Application.Run(new frmMemu());
         }
     }
 }
